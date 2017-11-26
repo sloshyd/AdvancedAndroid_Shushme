73274e36b25897ff51b46bcfb7f149f003de356a
sloshyD <uk.darren.hardesty@gmail.com>
2017-11-26 10:08:35 +0000

    Completed ToDos
     Added the permissions (uses-permissions) in manifest
    Added meta-tag to manifest (need to have generated a app level API key)
    Add Play-services-locations and Play-services-places dependencies
    Create a GoogleClientApi with GEO_APP_API and LocationServices.API
    Implement interfaces GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
    Implement methods onConnected(), onConnectionSuspended(), onConnectionFailed()
    Changed Layout to include checkbox
    Setup the checkbox in the onResume() (as needs to check for permissions after app loses focus)

--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -16,15 +16,30 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     // Constants
     public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;// code added to array when new location is added
 
     // Member variables
     private PlaceListAdapter mAdapter;
@@ -47,11 +62,68 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
         // TODO (4) Create a GoogleApiClient with the LocationServices API and GEO_DATA_API
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "Google Play Services - Connection Established");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Google Play Services - Connection Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Google Play Services - Connection Failed");
     }
 
     // TODO (5) Override onConnected, onConnectionSuspended and onConnectionFailed for GoogleApiClient
     // TODO (7) Override onResume and inside it initialize the location permissions checkbox
     // TODO (8) Implement onLocationPermissionClicked to handle the CheckBox click event
     // TODO (9) Implement the Add Place Button click event to show  a toast message with the permission status
+
+    //need to initialize the on permissions checkbox correctly in the onResume() not onCreate()
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        CheckBox permissionCheckbox = findViewById(R.id.location_permission_checkbox);
+
+        //check if permission is set if not set checkbox to blank
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            permissionCheckbox.setChecked(false);
+        }else {
+            permissionCheckbox.setChecked(true);
+            permissionCheckbox.setEnabled(false);//Stops user unchecking permission (even though nothing would happen)
+
+        }
+    }
+
+    public void onLocationPermissionClicked (View v){
+        //when user clicks to allow permission
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_FINE_LOCATION);
+    }
+
+    public void onAddPlaceButtonClicked (View v){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(this, getString(R.string.location_permissions_granted_message), Toast.LENGTH_LONG).show();
+    }
 
 }
